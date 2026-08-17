2a9e9586d9119fb8c7c526e38be513768680af42
.email.com <gangisettymani98@gmail.com>
2026-08-17 22:59:17 +0530

    if i want create sg file

---
+++ locals.tf
@@ -0,0 +1,11 @@
+locals {
+  common_name = "${var.project}-${var.environment}-${var.sg_name}"
+  common_tags = {
+    project = var.project
+    environment =var.environment
+    terraform = true
+    Name = local.common_name
+
+  }
+
+}
---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "main" {
+  name        = local.common_name
+  description = "Allow traffic for ${var.sg_name} project ${var.project} in ${var.environment}"
+  vpc_id      = var.vpc_id
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+   tags = merge(
+     var.sg_name
+    local.common_name
+  )
+}
+
+
+ 
